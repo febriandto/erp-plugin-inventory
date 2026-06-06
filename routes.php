07abd2c3e30d907482f8468c2abbb45acb493af6
febriandto <febri031@gmail.com>
2026-06-06 15:44:08 +0700

    fix: static routes before dynamic to prevent 'create' matching as {product}

--- routes.php
+++ routes.php
@@ -4,18 +4,11 @@ use Illuminate\Support\Facades\Route;
 use Plugins\inventory\Controllers\ProductController;
 
 Route::prefix('inventory')->name('inventory.')->group(function () {
-
-    Route::middleware('can:inventory.view')->group(function () {
-        Route::get('products',           [ProductController::class, 'index'])->name('products.index');
-        Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
-    });
-
-    Route::middleware('can:inventory.manage')->group(function () {
-        Route::get('products/create',           [ProductController::class, 'create'])->name('products.create');
-        Route::post('products',                 [ProductController::class, 'store'])->name('products.store');
-        Route::get('products/{product}/edit',   [ProductController::class, 'edit'])->name('products.edit');
-        Route::put('products/{product}',        [ProductController::class, 'update'])->name('products.update');
-        Route::delete('products/{product}',     [ProductController::class, 'destroy'])->name('products.destroy');
-    });
-
+    Route::get('products',                  [ProductController::class, 'index'])->name('products.index')->middleware('can:inventory.view');
+    Route::get('products/create',           [ProductController::class, 'create'])->name('products.create')->middleware('can:inventory.manage');
+    Route::post('products',                 [ProductController::class, 'store'])->name('products.store')->middleware('can:inventory.manage');
+    Route::get('products/{product}',        [ProductController::class, 'show'])->name('products.show')->middleware('can:inventory.view');
+    Route::get('products/{product}/edit',   [ProductController::class, 'edit'])->name('products.edit')->middleware('can:inventory.manage');
+    Route::put('products/{product}',        [ProductController::class, 'update'])->name('products.update')->middleware('can:inventory.manage');
+    Route::delete('products/{product}',     [ProductController::class, 'destroy'])->name('products.destroy')->middleware('can:inventory.manage');
 });
